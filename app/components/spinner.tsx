5b4a272732a40d4f833e950671b8a11fa57b4aa7
SamyCode <sambea2103@gmail.com>
2023-11-27 21:43:35 -0500

    create spinner component

---
+++ app/components/spinner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+export default function Spinner() {
+  return (
+    <section className="flex justify-center items-center w-full">
+      <div>
+        <Image
+          src="./spinner.svg"
+          alt="spinner"
+          width={56}
+          height={56}
+          className="object-contain"
+        />
+      </div>
+  </section>
+  )
+}
